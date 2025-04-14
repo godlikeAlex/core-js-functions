20360ebc414ae4399087c69f82b887ffbd02f379
godlikeAlex <godlikedesigner@gmail.com>
2025-04-14 15:49:49 +0500

    feat: resolve partialUsingArguments

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -221,8 +221,10 @@ function logger(func, logFunc) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return (...innerArgs) => {
+    return fn.apply(this, [...args1, ...innerArgs]);
+  };
 }
 
 /**
