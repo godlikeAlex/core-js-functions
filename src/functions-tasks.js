49d61b68fe062888ce40c1b2197a3e5f041bbbb8
godlikeAlex <godlikedesigner@gmail.com>
2025-04-14 13:50:53 +0500

    feat: memoize

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -125,8 +125,18 @@ function getPolynom(...polynomArguments) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let cache = null;
+
+  return () => {
+    if (cache) return cache;
+
+    const result = func();
+
+    cache = result;
+
+    return result;
+  };
 }
 
 /**
