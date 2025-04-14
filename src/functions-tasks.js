29592ab9fb6ba64f50fb539f357932ada9544543
godlikeAlex <godlikedesigner@gmail.com>
2025-04-14 14:13:35 +0500

    feat: resolve retry task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -154,8 +154,20 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return () => {
+    let result;
+
+    for (let i = 0; i < attempts; i += 1) {
+      try {
+        result = func();
+      } catch (error) {
+        result = error;
+      }
+    }
+
+    return result;
+  };
 }
 
 /**
